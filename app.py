420c7def5ba9a069348206c4c3d667823062e213
Hsuan-Yu Lin <o100249243@gmail.com>
2019-03-06 13:12:07 -0700

    success to grad data

--- app.py
+++ app.py
@@ -6,19 +6,24 @@ import scrape_mars
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_data_db
+db.mars_data_coll.drop()
 
 @app.route("/")
 def home():
-    mars_data = mongo.db.collection.find_one()
+    mars_data = list(db.mars_data_coll.find())
+    print(mars_data)
     return render_template("index.html", mars_data = mars_data)
 
 @app.route('/scrape')
 def scrape():
-    mars_data = mongo.db.mars_data
     mars_data = scrape_mars.scrape()
-    mars_data.update({}, mars_data, upsert=True)
-    return redirect("/", code=302)
+    db.mars_data_coll.update({}, mars_data, upsert=True)
+    return redirect("/")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -4,66 +4,78 @@ import os
 import pandas as pd
 import time
 
+
 def init_browser():
 
-    execute_path = {"executable_path" : "C:/Users/Sam Lin/Desktop/UoA_BC/chromedriver"}
+    execute_path = {
+        "executable_path": "C:/Users/Sam Lin/Desktop/UoA_BC/chromedriver"}
     return Browser('chrome', **execute_path, headless=False)
 
+
 def scrape():
-    browser = init_browser()
-    mars_data = {}
+     browser = init_browser()
+     mars_data = {}
 
-    url = "https://mars.nasa.gov/news/"
-    browser.visit(url)
+     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+     browser.visit(url)
+     time.sleep(2)
 
-    html = browser.html
-    soup = bs(html,"html.parser")
+     html = browser.html
+     soup = bs(html,"html.parser")
+     
+     title = soup.find("div", class_="content_title").text
+     content = soup.find("div", class_="article_teaser_body").text
+     print(f"Title: {title}")
+     print(f"Content: {content}")
+     mars_data["News_Title"] = title
+     mars_data["News_Paragraph"] = content
 
-    title = soup.find("div", class_="content_title").get_text
-    content = soup.find("div", class_="article_teaser_body").get_text
-    print(f"Title: {title}")
-    print(f"Content: {content}")
-    mars_data["News_Title"] = title
-    mars_data["News_Paragraph"] = content
+     base_url = "https://www.jpl.nasa.gov/"
+     url2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+     browser.visit(url2)
+     time.sleep(2)
 
-    base_url = "https://www.jpl.nasa.gov/"
+     browser.click_link_by_id("full_image")
+     time.sleep(2)
 
-    html_image = browser.html
-    soup = bs(html_image, "html.parser")
-    img_url = soup.find("img", class_="fancybox-image")["src"]
-    featured_img_url = base_url + img_url
-    print(featured_img_url)
-    mars_data["Featured_Image"] = featured_img_url
+     html_image = browser.html
+     soup = bs(html_image, "html.parser")
 
-    weather_url = "https://twitter.com/marswxreport?lang=en"
-    browser.visit(weather_url)
+     img_url = soup.find("img", class_="fancybox-image")["src"]
+     featured_img_url = base_url + img_url
+     print(featured_img_url)
+     mars_data["Featured_Image"] = featured_img_url
 
-    html_weather = browser.html
-    soup = bs(html_weather, "html.parser")
+     weather_url = "https://twitter.com/marswxreport?lang=en"
+     browser.visit(weather_url)
+     time.sleep(2)
 
-    mars_weather = soup.find("p", class_="TweetTextSize").text
-    mars_weather = mars_weather.partition('pic')
-    print(mars_weather)
-    mars_data["Weather"] = mars_weather
+     html_weather = browser.html
+     soup = bs(html_weather, "html.parser")
 
-    fact_url = "https://space-facts.com/mars/"
+     mars_weather = soup.find("p", class_="TweetTextSize").text
+     mars_weather = mars_weather.partition('pic')
+     print(mars_weather)
+     mars_data["Weather"] = mars_weather
 
-    fact = pd.read_html(fact_url)
-    mars_facts_df = fact[0]
-    mars_facts_df.columns = ["Parameter", "Values"]
-    mars_facts_df
+     fact_url = "https://space-facts.com/mars/"
 
-    mars_html_table = mars_facts_df.to_html()
-    mars_html_table = mars_html_table.replace("\n", "")
-    mars_html_table
-    mars_data["Table"] = mars_html_table
+     fact = pd.read_html(fact_url)
+     mars_facts_df = fact[0]
+     mars_facts_df.columns = ["Parameter", "Values"]
+     mars_facts_df
 
-    hemisphere_image_urls = [
-     {"title": "Valles Marineris Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"},
-     {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
-      {"title": "Schiaparelli Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
-     {"title": "Syrtis Major Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
-    ]
-    mars_data["Hemisphere"] = hemisphere_image_urls
+     mars_html_table = mars_facts_df.to_html()
+     mars_html_table = mars_html_table.replace("\n", "")
+     mars_html_table
+     mars_data["Table"] = mars_html_table
 
-    return mars_data
+     hemisphere_image_urls = [
+          {"title": "Valles Marineris Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"},
+          {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
+          {"title": "Schiaparelli Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
+          {"title": "Syrtis Major Hemisphere", "img_url": "http://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
+     ]
+     mars_data["Hemisphere"] = hemisphere_image_urls
+
+     return mars_data
